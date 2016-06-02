4a610680222c08b0ad9553244010cca8a1015963
Raymond Chou <Raymond Chou@sickkids.ca>
2016-06-02 11:46:20 -0400

    Established Connection with MongoDB

--- Proj3/Proj3.go
+++ Proj3/Proj3.go
@@ -6,7 +6,15 @@ import (
 	"io"
 	"log"
 	"os"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type Student struct { //exported Student
+	Name  string
+	Phone string
+}
 
 func readFile(file io.Reader) {
 	scanner := bufio.NewScanner(file) // returns a new Scanner
@@ -27,6 +35,24 @@ func openFile(filename string) *os.File {
 	return file
 }
 
+func mongoconnect() {
+	session, err := mgo.Dial("localhost") //attempts a connection and creates a session
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	connection := session.DB("test").C("students") //name of DB and Collection
+	err = connection.Insert(&Student{"Ale", "+55 53 8116 9639"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := Student{}
+	err = connection.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Phone:", result.Phone)
+}
 func main() {
 
 	filename := "updatingDocuments.js"
@@ -34,6 +60,6 @@ func main() {
 	file := openFile(filename) // Opens specified file
 
 	defer file.Close() // close the file after everything has been executed, placed here so not forgotten
-
-	readFile(file) //reads specified file
+	mongoconnect()     //establish a connection to MongoDB on localhost
+	readFile(file)     //reads specified file
 }
